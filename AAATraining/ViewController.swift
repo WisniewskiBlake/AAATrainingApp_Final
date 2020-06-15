22daf764d8db8da048689f1b647285ade16549d9
WisniewskiBlake <bmwisnie@svsu.edu>
2020-06-14 21:04:07 -0400

    Initial Commit

---
+++ AAATraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 6/14/20.
+//  Copyright © 2020 Margaret Dwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
