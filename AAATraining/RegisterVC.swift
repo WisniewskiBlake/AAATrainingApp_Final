3c697b89d82b7534134460642d58ee8e4b25049b
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-06-19 03:30:52 -0400

    added constraints and outlets

--- AAATraining/RegisterVC.swift
+++ AAATraining/RegisterVC.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class RegisterVC: UIViewController {
+    
+    @IBOutlet weak var contentView_width: NSLayoutConstraint!
+    @IBOutlet weak var emailView_width: NSLayoutConstraint!
+    @IBOutlet weak var nameView_width: NSLayoutConstraint!
+    @IBOutlet weak var passwordView_width: NSLayoutConstraint!
+    @IBOutlet weak var birthdayView_width: NSLayoutConstraint!
+    @IBOutlet weak var genderView_width: NSLayoutConstraint!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // adjust width of the views to the screen of the device
+        contentView_width.constant = self.view.frame.width * 5
+        emailView_width.constant = self.view.frame.width
+        nameView_width.constant = self.view.frame.width
+        passwordView_width.constant = self.view.frame.width
+        birthdayView_width.constant = self.view.frame.width
+        genderView_width.constant = self.view.frame.width
     }
     
 
