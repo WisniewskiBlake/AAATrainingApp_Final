a56b3f7eaa8ccb5c5f56e59323442eec9de49ce7
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-06-19 00:39:45 -0400

    created 2 register storyboards

---
+++ AAATraining/RegisterVCTest.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterVCTest.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 6/19/20.
+//  Copyright © 2020 Margaret Dwan. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVCTest: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
