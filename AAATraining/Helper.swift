6e27f7e91805ed05a5759462b80acc86c2e475e3
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-06-23 02:57:20 -0400

    added instantiateVC helper method

--- AAATraining/Helper.swift
+++ AAATraining/Helper.swift
@@ -77,6 +77,17 @@ class Helper {
         alert.addAction(ok)
         vc.present(alert, animated: true, completion: nil)        
     }
+    
+    // allows us to go to another ViewController programmatically
+    func instantiateViewController(identifier: String, animated: Bool, by vc: UIViewController, completion: (() -> Void)?) {
+        
+        // accessing any ViewController from Main.storyboard via ID
+        let newViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier)
+        
+        // presenting accessed ViewController
+        vc.present(newViewController, animated: animated, completion: completion)
+        
+    }
 
     
     
