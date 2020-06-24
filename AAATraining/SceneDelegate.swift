4bb6ea7cd366acaf0c9a595ca89dd8c6eddecf83
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-06-24 03:01:07 -0400

    register working - login working (can't put Null from DB into NSUserdefaults)

--- AAATraining/SceneDelegate.swift
+++ AAATraining/SceneDelegate.swift
@@ -16,7 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).      
+        
         currentUser = DEFAULTS.object(forKey: keyCURRENT_USER) as? Dictionary<String, Any>
         if currentUser != nil {
         goToMain()
