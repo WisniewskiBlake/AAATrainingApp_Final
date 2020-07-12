fea212f1909ea3979a4ba242a87e11df9dcefde9
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-07-12 05:26:08 -0400

    created cell class

---
+++ AAATraining/CoachViewControllers/CoachPicCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoachPicCell.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 7/12/20.
+//  Copyright © 2020 Margaret Dwan. All rights reserved.
+//
+
+import UIKit
+
+class CoachPicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
