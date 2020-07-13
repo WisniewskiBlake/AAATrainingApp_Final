480e364cf945aa781f60fefa723c8fdcdcf9a6fb
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-07-13 06:48:11 -0400

    created roster VC files for coach and player

---
+++ AAATraining/PlayerViewControllers/PlayerRosterVC.swift
@@ -0,0 +1,30 @@
+//
+//  PlayerRosterVC.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 7/13/20.
+//  Copyright © 2020 Margaret Dwan. All rights reserved.
+//
+
+import UIKit
+
+class PlayerRosterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
