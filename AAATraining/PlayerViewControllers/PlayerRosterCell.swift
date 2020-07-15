b85b101808d5b2537f97e4ec9a98863b9b0e3e1e
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-07-15 01:37:14 -0400

    created custom Player Roster Cell

---
+++ AAATraining/PlayerViewControllers/PlayerRosterCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerRosterCell.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 7/15/20.
+//  Copyright © 2020 Margaret Dwan. All rights reserved.
+//
+
+import UIKit
+
+class PlayerRosterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
