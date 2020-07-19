be77b120487b9b79504bfb685dc4b3234ff643f6
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-07-19 01:27:44 -0400

    added firebase files

--- AAATraining/AppDelegate.swift
+++ AAATraining/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 // global var - to store all logged / registered user infromation
 
@@ -25,6 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
         
         // loading current user
         currentUser = DEFAULTS.object(forKey: "currentUser") as? Dictionary<String, Any>
