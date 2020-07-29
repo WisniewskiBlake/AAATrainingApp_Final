59556814c44033896fe8147b51d03b84a5b238ba
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-07-28 23:44:00 -0400

    added event controller coach

---
+++ AAATraining/CoachViewControllers/Event_Coach.swift
@@ -0,0 +1,25 @@
+//
+//  Event_Coach.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 7/28/20.
+//  Copyright © 2020 Margaret Dwan. All rights reserved.
+//
+
+import UIKit
+
+class Event_Coach: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
