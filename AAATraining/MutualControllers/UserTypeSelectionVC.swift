75e86533d23290d8f2873a20bab6419232aa257b
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-07-30 11:36:44 -0400

    re adjusted storyboard

--- AAATraining/MutualControllers/UserTypeSelectionVC.swift
+++ AAATraining/MutualControllers/UserTypeSelectionVC.swift
@@ -155,7 +155,7 @@ extension UserTypeSelectionVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+        return 50
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
