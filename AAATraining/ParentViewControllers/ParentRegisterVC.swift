8fda853e5856468d2b46125feaa9df05368f9e12
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-08-03 01:09:14 -0400

    created parent outlets

--- AAATraining/ParentViewControllers/ParentRegisterVC.swift
+++ AAATraining/ParentViewControllers/ParentRegisterVC.swift
@@ -10,12 +10,27 @@ import UIKit
 
 class ParentRegisterVC: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailContinueButton: UIButton!
+    @IBOutlet weak var nameContinueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-
+    
+    @IBAction func emailContinueClicked(_ sender: Any) {
+    }
+    
+    @IBAction func nameContinueClicked(_ sender: Any) {
+    }
+    
+    @IBAction func textFieldDidChange(_ sender: UITextField) {
+    }
+    
 }
