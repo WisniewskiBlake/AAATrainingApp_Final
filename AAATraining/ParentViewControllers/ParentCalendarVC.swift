16ba2eb7d0ae0c00a6aa0d81fd26be36c3263e54
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-08-04 10:53:52 -0400

    complete parent calendar

--- AAATraining/ParentViewControllers/ParentCalendarVC.swift
+++ AAATraining/ParentViewControllers/ParentCalendarVC.swift
@@ -23,7 +23,91 @@ class ParentCalendarVC: UIViewController, FSCalendarDelegate, FSCalendarDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        NotificationCenter.default.addObserver(self, selector: #selector(loadEvents), name: NSNotification.Name(rawValue: "createEvent"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(loadEvents), name: NSNotification.Name(rawValue: "deleteEvent"), object: nil)
+
+        calendar.delegate = self
+        loadEvents()
         
+    }
+    
+    // pre-load func
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+       loadEvents()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        recentListener.remove()
+    }
+    
+    @objc func loadEvents() {
+        ProgressHUD.show()
+        recentListener = reference(.Event).addSnapshotListener({ (snapshot, error) in
+                           
+            self.allEvents = []
+            self.allEventDates = []
+                        
+            if error != nil {
+                print(error!.localizedDescription)
+                ProgressHUD.dismiss()
+                self.calendar.reloadData()
+                return
+            }
+            guard let snapshot = snapshot else { ProgressHUD.dismiss(); return }
+                   
+            if !snapshot.isEmpty {
+                for eventDictionary in snapshot.documents {
+                    
+                   let eventDictionary = eventDictionary.data() as NSDictionary
+                   let event = Event(_dictionary: eventDictionary)
+                   self.allEvents.append(event)
+                    self.allEventDates.append(event.eventDate)
+                
+               }
+               self.calendar.reloadData()
+            
+           }
+            self.calendar.reloadData()
+            ProgressHUD.dismiss()
+        })
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        calendar.formatter.dateFormat = "EEEE, MM-dd-YYYY"
+        let dateString = calendar.formatter.string(from: date)
+        
+        let eventVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Event_Coach") as! Event_Coach
+        let navController = UINavigationController(rootViewController: eventVC)
+        
+        for event in allEvents {
+            if event.eventDate == dateString {
+                //eventVC.eventText = event.eventText
+                eventVC.updateNeeded = true
+                eventVC.eventID = event.eventID
+                eventVC.event = event
+            } else {
+                eventVC.eventText = ""
+            }
+        }
+        
+        eventVC.hidesBottomBarWhenPushed = true
+        eventVC.dateString = dateString
+        
+        self.navigationController?.present(navController, animated: true, completion: nil)
+    }
+    
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillDefaultColorFor date: Date) -> UIColor? {
+        calendar.formatter.dateFormat = "EEEE, MM-dd-YYYY"
+        let dateString = calendar.formatter.string(from: date)
+        
+        if allEventDates.contains(dateString) {
+            return #colorLiteral(red: 0.1006183103, green: 0.2956552207, blue: 0.71825701, alpha: 1)
+            
+        } else {
+            return nil
+        }
+        
+
     }
     
 
