79a12d4ee68ec416c0d9564587549d7f9b917083
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-08-08 00:59:58 -0400

    created outlets for parent event

--- AAATraining/ParentViewControllers/ParentEvent.swift
+++ AAATraining/ParentViewControllers/ParentEvent.swift
@@ -7,24 +7,27 @@
 //
 
 import UIKit
+import ProgressHUD
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
 
-class ParentEvent: UIViewController {
-
+class ParentEvent: UIViewController, UITextViewDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var placeHolderLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonPressed(_ sender: Any) {
+        
     }
-    */
+    
+    
 
 }
---
+++ AAATraining/ParentViewControllers/ParentRecentChat.swift
@@ -0,0 +1,22 @@
+//
+//  ParentRecentChat.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 8/8/20.
+//  Copyright © 2020 Margaret Dwan. All rights reserved.
+//
+
+import UIKit
+
+class ParentRecentChat: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
