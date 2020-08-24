56fc3d206791672fdc743d1aae3671742b0e3f64
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-08-23 21:32:56 -0400

    added tap method for register Team label

--- AAATraining/MutualControllers/TeamLoginVC.swift
+++ AAATraining/MutualControllers/TeamLoginVC.swift
@@ -7,24 +7,33 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+import ProgressHUD
 
 class TeamLoginVC: UIViewController {
+    
+    @IBOutlet weak var teamCodeText: UITextField!
+    @IBOutlet weak var registerTeamLabel: UILabel!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    let registerTeamTapGestureRecognizer = UITapGestureRecognizer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        registerTeamTapGestureRecognizer.addTarget(self, action: #selector(self.registerTeamClicked))
+        registerTeamLabel.isUserInteractionEnabled = true
+        registerTeamLabel.addGestureRecognizer(registerTeamTapGestureRecognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func continueButtonClicked(_ sender: Any) {
+        
     }
-    */
+    
+    @objc func registerTeamClicked() {
+        
+    }
+
 
 }
