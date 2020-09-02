9a3f3b33732d875b6eafae6d875b210994a828dc
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-09-01 22:37:44 -0400

    added color picker VC

---
+++ AAATraining/CoachViewControllers/ColorPickerVC.swift
@@ -0,0 +1,30 @@
+//
+//  ColorPickerVC.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 9/1/20.
+//  Copyright © 2020 Margaret Dwan. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
