c757698efcaeeb2f497506365b0d1476aefecc5b
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-09-25 23:35:16 -0400

    multi event cell in place

---
+++ AAATraining/CoachViewControllers/MultiEvent_Coach.swift
@@ -0,0 +1,37 @@
+//
+//  MultiEvent_Coach.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 9/25/20.
+//  Copyright © 2020 Blake Wisniewski. All rights reserved.
+//
+
+import UIKit
+
+class MultiEvent_Coach: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+
+}
