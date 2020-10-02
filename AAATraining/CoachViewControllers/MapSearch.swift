24c65471ace606a1d1dd0c12de6f09c0519f8fba
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-10-02 08:19:46 -0400

    created map search controller

---
+++ AAATraining/CoachViewControllers/MapSearch.swift
@@ -0,0 +1,31 @@
+//
+//  MapSearch.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 10/2/20.
+//  Copyright © 2020 Blake Wisniewski. All rights reserved.
+//
+
+import UIKit
+
+class MapSearch: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+}
