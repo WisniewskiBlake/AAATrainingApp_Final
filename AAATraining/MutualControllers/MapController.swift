50bcff6dfb10c0ba6645c41c24ff57c661ad7329
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-10-02 08:30:09 -0400

    created map controller

---
+++ AAATraining/MutualControllers/MapController.swift
@@ -0,0 +1,24 @@
+//
+//  MapController.swift
+//  AAATraining
+//
+//  Created by Margaret Dwan on 10/2/20.
+//  Copyright © 2020 Blake Wisniewski. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
